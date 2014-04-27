179f774308322e6e83273e473cf0abd7fc88cf44
mloreti <mloreti@users.noreply.github.com>
2014-04-27 10:49:56 -0400

    Create index.php

---
+++ wod/index.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Web On Drugs</title>
+    <!-- Bootstrap -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body onLoad="pageScroll()">
+<div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+       <div class="topspace">
+        <a class="brand" href="http://web.art.fsu.edu/mundy/2012f_intmedia/loreti/drugs/">Web on drugs</a>
+              <form class="form-search" action="index.php" method="post">
+          <input type="text" name="url" class="input-medium search-query">
+          <button type="submit" class="btn">Go!</button>
+        </form>
+    
+        </div>
+      </div>
+    </div>
+    
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <audio autoplay loop>
+          <source src="music1.mp3">
+      </audio>    
+    
+    
+        <script>
+    
+    
+    function pageScroll() {
+        	window.scrollBy(0,10);  
+        	scrolldelay = setTimeout('pageScroll()',500); // scrolls every 100 milliseconds
+    }
+    
+    
+    alert("Dude, dont click ok...");
+    
+    if(frames.length<1){void(document.bgColor=('yellow'))}else{alert('you suck)')}
+    
+    
+    
+    javascript:R=10; x1=.3; y1=.05; x2=.25; y2=.24; x3=6; y3=4; x4=9; y4=9; x5=5; y5=9; 
+    var DI= document.getElementsByTagName("img"); DIL=DI.length; 
+    function A(){for(i=0; i<DIL; i++){DIS=DI[ i ].style; DIS.position='relative'; 
+    DIS.left=Math.sin(R*x1+i*x2+x3)*x4+x5+"px"; 
+    DIS.top=Math.cos(R*y1+i*y2+y3)*y4+y5+"px"}R++}tag=setInterval('A()',5 );
+    
+    javascript:function Shw(n) {if (self.moveBy) {for (i = 50; i > 0; i--) 
+    {for (j = n; j > 0; j--) {self.moveBy(1,i);self.moveBy(i,0);self.moveBy(0,-i);
+    self.moveBy(-i,0); } } }} Shw(6)-accident
+    
+    
+    
+    </script>
+    <script>javascript: x = function() { 
+   document.body.style.backgroundImage = "url(http://www.mytinyphone.com/uploads/users/gurkis/sm2/97617.gif)" 
+}; 
+x()</script>
+    <script>javascript:(function(){var n_to_open,dl,dll,i; function linkIsSafe(u) { if (u.substr(0,7)=='mailto:') return false; if (u.substr(0,11)=='javascript:') return false; return true; } n_to_open = 0; dl = document.links; dll = dl.length; for(i = 0; i < dll; ++i) { if (linkIsSafe(dl[i].href)) ++n_to_open; } if (!n_to_open) alert ('no links'); else { if (confirm('Open ' + n_to_open + ' links in new windows?')) for (i = 0; i < dll; ++i) if (linkIsSafe(dl[i].href)) window.open(dl[i].href); } })();</script>
+    <script>javascript:var coding = "abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM"; function rot13(t) { for (var r = "",i=0;i<t.length;i++) { character = t.charAt(i); position = coding.indexOf(character); if (position > -1) character = coding.charAt(position + 13); r += character; } return r; } S=window.getSelection(); function t(N) { return N.nodeType == N.TEXT_NODE; } function r(N) { if (t(N)) N.data = rot13(N.data); } for (j=0;j<S.rangeCount;++j) { var g=S.getRangeAt(j), e=g.startContainer, f=g.endContainer, E=g.startOffset, F=g.endOffset, m=(e==f); if(!m||!t(e)) { /* rot13 each text node between e and f, not including e and f. */ q=document.createTreeWalker(g.commonAncestorContainer, NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT, null, false); q.currentNode=e; for(N=q.nextNode(); N && N != f; N = q.nextNode()) r(N); } if (t(f)) f.splitText(F); if (!m) r(f); if (t(e)) { r(k=e.splitText(E)); if(m)f=k; e=k;} if (t(f)) g.setEnd(f,f.data.length); } void 0</script>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html> 
